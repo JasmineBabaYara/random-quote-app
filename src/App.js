b2b3e627bb1d526dad1698588d0320ae8ace6155
JasmineBabaYara <jassyprincess1@gmail.com>
2021-09-29 10:12:08 -0700

    random_quote-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+  super()
+  this.state={
+    quote:"",
+    author:""
+  }
+
+  this.randomNum=this.randomNum.bind(this);
+  }
+
+  componentDidMount(){
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data)
+      data = data[this.randomNum(0, data.length - 1)]
+      this.setState({
+       quote:data.text,
+       author:data.author
+      })
+    })
+    .catch(error => console.log('Error', error));
+  }
+
+  randomNum(max,min){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  render(){
+    return(
+      <div>
+        <p className='quote'>{this.state.quote}</p>
+        <p className='author'>--{this.state.author}--</p>
+      </div>
+    )
+  }
 }
 
 export default App;
